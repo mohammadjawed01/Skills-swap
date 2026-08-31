d72ca6b63f1a300372f37e12feb3b0707a70e7b0
mohammadjawed01 <mohammadjaved1736@gmail.com>
2026-08-31 19:56:22 +0530

    Refactor theme toggle functionality and enhance logo hover effects

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let theme = document.querySelector("#theme");
-
 theme.addEventListener('click', function(){
     document.body.classList.toggle("dark");
-
     if (document.body.classList.contains("dark")) {
         theme.textContent = "☀";
     }
-
     else{
         theme.textContent = "🌙";
     }
 })
+
+let profile = document.querySelector("#profile");
+
 
